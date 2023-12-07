ad1de57fed8b2025076e41e0d7863057c8e2276d
Atul Kamble <atul_kamble@hotmail.com>
2023-12-07 10:49:37 +0530

    Added Project Files

---
+++ main.tf
@@ -0,0 +1,74 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.29.0"
+    }
+  }
+}
+
+resource "aws_default_vpc" "default" {
+
+}
+
+resource "aws_security_group" "http_server_sg" {
+  name   = "http_server_sg"
+  vpc_id = "vpc-082091840d0b84dfa"
+
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    name = "http_server_sg"
+  }
+}
+
+resource "aws_instance" "http_server" {
+  ami                    = "ami-0230bd60aa48260c6"
+  key_name               = "default-ec2"
+  instance_type          = "t2.medium"
+  subnet_id              = "subnet-0d57e95d890b899a8"
+  vpc_security_group_ids = [aws_security_group.http_server_sg.id]
+
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.aws_key_pair)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install httpd -y",
+      "sudo systemctl start httpd",
+      "sudo systemctl enable httpd",
+      "sudo chmod 777 /var/www/html",
+      "cd /var/www/html",
+      "git clone https://github.com/atulkamble/DevOpsProject.git",
+      "cd DevOpsProject",
+      "sudo cp index.html /var/www/html/index.html"
+
+    ]
+  }
+}
